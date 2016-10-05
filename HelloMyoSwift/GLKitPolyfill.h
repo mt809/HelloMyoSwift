13494844055e0a7b9d04d8997d97d329d07c16d7
Lahiru Lakmal Priyadarshana <llahiru@gmail.com>
2016-10-05 15:17:35 -0400

    Re Indent code

--- HelloMyoSwift/GLKitPolyfill.h
+++ HelloMyoSwift/GLKitPolyfill.h
@@ -4,28 +4,28 @@
 #import <MyoKit/MyoKit.h>
 
 @interface AccelerationData: NSObject
-  @property (nonatomic) float magnitude;
-  @property (nonatomic) float x;
-  @property (nonatomic) float y;
-  @property (nonatomic) float z;
+@property (nonatomic) float magnitude;
+@property (nonatomic) float x;
+@property (nonatomic) float y;
+@property (nonatomic) float z;
 @end
 
 @implementation AccelerationData
 @end
 
 @interface OrientationData: NSObject
-  @property (nonatomic) TLMAngle *pitch;
-  @property (nonatomic) TLMAngle *yaw;
-  @property (nonatomic) TLMAngle *roll;
+@property (nonatomic) TLMAngle *pitch;
+@property (nonatomic) TLMAngle *yaw;
+@property (nonatomic) TLMAngle *roll;
 @end
 
 @implementation OrientationData
 @end
 
 @interface GyroData: NSObject
-  @property (nonatomic) float x;
-  @property (nonatomic) float y;
-  @property (nonatomic) float z;
+@property (nonatomic) float x;
+@property (nonatomic) float y;
+@property (nonatomic) float z;
 @end
 
 @implementation GyroData
